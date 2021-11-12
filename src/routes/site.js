0897f66ba73a2833647f7e2bf97a8b0e9e0f5c74
fannatic2000 <55821590+fannatic2000@users.noreply.github.com>
2021-11-12 16:59:43 +0700

    Install prettier, lint-stage, husky

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-    router.use('/search', siteController.search);
+router.use('/search', siteController.search);
 
-        router.use('/', siteController.home);
+router.use('/', siteController.home);
 
 module.exports = router;
