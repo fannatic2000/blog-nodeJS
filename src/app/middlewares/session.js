1dad23edce19049a3eb55e53f18c03725610ca03
fannatic2000 <55821590+fannatic2000@users.noreply.github.com>
2022-01-23 17:06:57 +0700

    connect-mongodb-session

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -1,11 +1,23 @@
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 module.exports = function (app) {
+    var store = new MongoDBStore({
+        uri: 'mongodb://localhost:27017',
+        databaseName: 'f8_education_dev',
+        collection: 'mySessions',
+    });
+
+    store.on('error', function (error) {
+        // Also get an error here
+    });
+
     // Session
     var sess = {
         secret: 'SECRET_KEY',
         cookie: {},
-        resave: false,
+        store: store,
+        resave: true,
         saveUninitialized: true,
     };
     if (app.get('env') === 'production') {
